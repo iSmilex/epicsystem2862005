e716ab78348eae738e65b00184de1b6872f6a84a
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-09-07 17:58:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -246,7 +246,7 @@ ${member}
 // Autorole
 
 client.on('guildMemberAdd', member => {
-  member.addRole(member.guild.roles.find('name', 'Epic.));
+  member.addRole(member.guild.roles.find('name', 'Epic.'));
 });
 
 client.login(process.env.BOT_TOKEN);
