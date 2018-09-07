1161e544a117fc7e3b205cafc540e3d50bd5fbce
iSmilex <38261980+iSmilex@users.noreply.github.com>
2018-09-07 17:22:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -233,14 +233,57 @@ client.on('guildMemberAdd', member => {
   let embed = new Discord.RichEmbed()
   .setThumbnail(memberavatar)
   .setColor('RANDOM')
-  .setDescription(`
-  **Welcome ** ${member} ** to __Epic Server__, Hope you have a nice time **
-  You're The ${member.guild.members.size}'s Member.
+  .setDescription(`**
+منور سيرفر ايبك يا عسل :two_hearts:
+${member}
+أتمنى تشوف روم : <#484070872697667584> , <@486892282264682506>
   `)
   .setFooter(member.guild.name)
-  .setImage("https://media.giphy.com/media/OkJat1YNdoD3W/giphy.gif")
   channel.sendEmbed(embed);
 });
 
+
+//
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("480895922281381898")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'epic');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("480895922281381898")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(`** Welcome to Epic Server , Invited By ${Invite.inviter} **`)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 client.login(process.env.BOT_TOKEN);
